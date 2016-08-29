8f6afd82b8256aac6f2c2acb836cd6099a1c51e5
Qasim Ali <5zari@boun.cr>
2016-08-28 21:51:11 -0400

    static dialog

--- ff-tab-switcher.js
+++ ff-tab-switcher.js
@@ -1,1 +1,20 @@
-alert('hallo world');
+// alert(chrome.extension.getURL("selector.html"));
+
+// create our content frame
+var frame = document.createElement('iframe');
+frame.src = chrome.extension.getURL("selector.html");
+
+// style - why can't I overlay an element and ignore the page's CSS
+Object.assign(frame.style, {
+	position: "fixed",
+	top: "50%",
+	left: "50%",
+	"z-index": "9999999",
+	transform: "translateX(-50%) translateY(-50%)",
+	border: "1px solid #a6a6a6",
+	background: "#232323",
+	"border-radius": "10px",
+	"box-shadow" : "0 0 25px #000"
+});
+
+document.body.appendChild(frame);
